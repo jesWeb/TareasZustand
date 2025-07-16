f1911042c564ffb9a307beb19faf7161223b47b0
jesus Gutierrez <jesuslgutierrez685@gmail.com>
2025-07-15 18:23:39 -0600

    agrego el local storage pero esta vez con zustand

--- src/types/store.ts
+++ src/types/store.ts
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import type { DraftTarea, Formulario } from ".";
 import { v4 as uuidv4 } from "uuid"
-import { devtools } from "zustand/middleware";
+import { devtools, persist } from "zustand/middleware";
 
 type TareasState = {
     tareas: Formulario[],
@@ -22,40 +22,43 @@ const creearTarea = (tarea: DraftTarea): Formulario => {
 
 export const useTareaStore = create<TareasState>()(
     devtools(
-        (set) => ({
-            //estado inicial 
-            tareas: [],
-            activarId: '',
-            agregarTarea(data) {
-                console.log("llega la data del formulario de store", data);
-                const nuvaTarea = creearTarea(data)
-                set((state) => ({
-                    tareas: [
-                        ...state.tareas,
-                        nuvaTarea
-                    ]
-                }))
-            },
-            eliminarTarea: (id) => {
-                console.log("Recibimos el id para eliminar la tarea en el store", id);
-                set((state) => ({
-                    tareas: state.tareas.filter((tarea) => tarea.id !== id)
-                }))
+        persist(
+            (set) => ({
+                //estado inicial 
+                tareas: [],
+                activarId: '',
+                agregarTarea(data) {
+                    console.log("llega la data del formulario de store", data);
+                    const nuvaTarea = creearTarea(data)
+                    set((state) => ({
+                        tareas: [
+                            ...state.tareas,
+                            nuvaTarea
+                        ]
+                    }))
+                },
+                eliminarTarea: (id) => {
+                    console.log("Recibimos el id para eliminar la tarea en el store", id);
+                    set((state) => ({
+                        tareas: state.tareas.filter((tarea) => tarea.id !== id)
+                    }))
 
-            },
-            obtenerTarea: (id) => {
-                console.log('verificamos que llege el id en store,[para editarlo', id);
-                set(() => ({
-                    activarId: id
-                }))
-            },
-            ActualizarTarea: (data) => {
-                set((state) => ({
-                    tareas: state.tareas.map((tarea) => tarea.id === state.activarId ? { id: state.activarId, ...data } : tarea),
-                    activarId: '',
-                }))
-            },
+                },
+                obtenerTarea: (id) => {
+                    console.log('verificamos que llege el id en store,[para editarlo', id);
+                    set(() => ({
+                        activarId: id
+                    }))
+                },
+                ActualizarTarea: (data) => {
+                    set((state) => ({
+                        tareas: state.tareas.map((tarea) => tarea.id === state.activarId ? { id: state.activarId, ...data } : tarea),
+                        activarId: '',
+                    }))
+                },
 
-        })
-    )
+            }),{
+              name:'tareaSorage'  
+            }
+        ))
 )
